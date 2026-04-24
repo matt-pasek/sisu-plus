9b927ea88d8c9cc275ead201d44f6737d1c731f0
matt-pasek <m_pasek@icloud.com>
2026-04-24 23:14:43 +0300

    fix: better cc for Sisu mode

--- src/app/controlCenter/ControlCenter.comp.tsx
+++ src/app/controlCenter/ControlCenter.comp.tsx
@@ -41,17 +41,19 @@ function Toggle({ checked, onChange }: { checked: boolean; onChange: (val: boole
 }
 
 export function ControlCenter() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const [prefs, setPrefs] = useChromeStorage();
+
+  const size = prefs.sisuPlusActive ? 80 : 56;
 
   return (
     <motion.div
       className="fixed right-4 bottom-4 z-50 overflow-hidden bg-[#0B0519] text-white shadow-2xl"
       animate={{
-        width: isOpen || isHovered ? 320 : 80,
-        height: isOpen ? 300 : 80,
-        borderRadius: isOpen ? 12 : 80,
+        width: isOpen || isHovered ? 320 : size,
+        height: isOpen ? 300 : size,
+        borderRadius: isOpen ? 12 : size,
       }}
       transition={{ type: 'spring', bounce: 0.1, duration: 0.6 }}
     >
@@ -135,7 +137,17 @@ export function ControlCenter() {
           onClick={() => setIsOpen(!isOpen)}
           className="flex h-20 w-20 cursor-pointer items-center justify-center p-2"
         >
-          <CircularText text="SISU PLUS * CONTROLS * " onHover="slowDown" spinDuration={20} />
+          {prefs.sisuPlusActive ? (
+            <CircularText text="SISU PLUS * CONTROLS * " onHover="slowDown" spinDuration={20} />
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-9">
+              <path
+                fillRule="evenodd"
+                d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          )}
         </button>
       </div>
     </motion.div>
